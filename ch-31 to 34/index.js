ecf78b3dda4096d3049f4b746c157acd9bbb597a
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-02-22 00:50:34 +0500

    Update index.js

--- ch-31 to 34/index.js
+++ ch-31 to 34/index.js
@@ -1,10 +1,33 @@
-var name = prompt("Please Enter your name ");
-var birthDate = prompt("Please Enter your DOB date ");
-var birthMonth = prompt("Please Enter your DOB month ");
-var birthYear = prompt("Please Enter your DOB year ");
+// var name = prompt("Please enter your full name");
+// var birthDate = Number(prompt("Please Enter your DOB date"));
+// var birthMonth = Number(prompt("Please Enter your DOB month"));
+// var birthYear = Number(prompt("Please Enter your DOB year"));
 
-var date = new Date().getDate(birthDate);
-var month = new Date().getMonth(birthMonth);
-var year = new Date().getFullYear(birthYear);
+// var getToday = new Date()
+// var getHours = getToday.getHours();
 
-console.log(`Hello ${name}, Good `)
+// if (getHours >= 0000 && getHours <= 1200) {
+//     var greet = (`Good Morning`);
+// } else if (getHours >= 1200 && getHours <= 1700) {
+//     var greet = (`Good Afternoon`);
+// } else if (getHours >= 1700 && getHours <= 2100) {
+//     var greet = (`Good Evening`)
+// } else if (getHours >= 2100 && getHours <= 2359) {
+//     var greet = (`Good Night`)
+// }
+
+// var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'oct', 'Nov', 'Dec']
+// var DOB = new Date(`${birthDate}/${months[birthMonth - 1]}/${birthYear}`);
+
+// var msdiff = getToday.getTime() - DOB.getTime();
+
+// var Years = Math.floor(msdiff / (1000 * 60 * 60 * 24 * 365));
+// var hours = Years * 12 * 30 * 24;
+// var days = Years * 12 * 30;
+
+// console.log(`Hello ${name} ${greet}`);
+// console.log(`Your DOB is ${DOB}`);
+// console.log(`You are ${Years} years old`);
+// console.log(`Your age in days are ${days}`);
+// console.log(`Your age in hours are ${hours}`);
+// console.log(`Your birthday duration days are`);
